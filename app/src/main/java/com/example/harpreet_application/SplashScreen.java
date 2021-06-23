135963974b8c7d5095bf1f0807db68034795394d
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-06-23 21:54:45 +0530

    this is my splash screen

---
+++ app/src/main/java/com/example/harpreet_application/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.harpreet_application;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        }).start();
+
+
+
+    }
+}
